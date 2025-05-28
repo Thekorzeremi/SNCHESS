a034a37894caed288c701673a39172aaabd7c795
Korzeremi02 <remi.k@cashflowpositif.fr>
2025-05-28 12:04:27 +0200

    ci: firebaseAuthentificationServoce.dart and modifying .analysis_option.yaml

---
+++ frontend/lib/services/firebaseAuthentificationService.dart
@@ -0,0 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+FirebaseAuth auth = FirebaseAuth.instance;
+
+class Firebaseauthentificationservice {
+  //temp variables
+  String email = "test@test.test";
+  String password = "test";
+
+  void checkIfUserIsConnected() {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print("User is signed out !");
+      } else {
+        print("User is signed in !");
+      }
+    });
+  }
+
+  void registerWithEmailAndPassword(String email, String password) async {
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void connectWithEmailAndPassword(String email, String password) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  void signOutCurrentUser() {
+    try {
+      FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
